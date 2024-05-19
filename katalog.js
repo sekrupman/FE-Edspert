7dee5da9c86a5f3871396b8ed45ad754c354377e
sekrupman <45067224+sekrupman@users.noreply.github.com>
2024-05-19 17:28:28 +0700

    first commit

---
+++ katalog.js
@@ -0,0 +1,167 @@
+const listElement = document.getElementById("courseContainer");
+const paginationElement = document.getElementById("pagination");
+const searchInput = document.querySelector(".form-control");
+
+// pagination
+let currentPage = 1;
+let rows = 3;
+let columns = 2;
+let itemsPerPage = rows * columns;
+let courses = [];
+
+// API
+async function initializeCourses() {
+  try {
+    const response = await axios.get(
+      "https://be-edspert.vercel.app/api/products"
+    );
+    const fetchedData = response.data;
+
+    if (fetchedData && fetchedData.products) {
+      courses = fetchedData.products;
+    } else {
+      courses = [];
+    }
+
+    displayList(courses, listElement, rows, columns, currentPage);
+    displayPagination();
+  } catch (error) {
+    console.error("Error initializing courses:", error);
+  }
+  console.log("response", courses);
+}
+
+// DISPLAY CARD
+function displayList(items, wrapper, rows, columns, page) {
+  wrapper.innerHTML = "";
+  page--;
+
+  const itemsPerPage = rows * columns;
+  const start = itemsPerPage * page;
+  const end = start + itemsPerPage;
+  const paginatedItems = items.slice(start, end);
+
+  for (let i = 0; i < paginatedItems.length; i++) {
+    const course = paginatedItems[i];
+
+    const column = document.createElement("div");
+    column.classList.add("col-md-3");
+    column.innerHTML = `
+            <div class="card mb-4">
+                <img src="./asset/kelas.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${course.title}</h5>
+                    <p class="card-text">Batch ${course.batch}</p>
+                    <ul>
+                    ${
+                      course.facility
+                        ? course.facility
+                            .map(
+                              (facility) =>
+                                `<li><img src="./asset/check-icon.png"> ${facility}</li>`
+                            )
+                            .join("")
+                        : ""
+                    }
+                    </ul>
+                    <div class="pricing">
+                    <del class="card-text price">Rp ${
+                      course.price
+                        ? course.price
+                            .toString()
+                            .replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+                        : "N/A"
+                    }</del>
+                    <h5 class="discount">Rp ${
+                      course.disc
+                        ? course.disc
+                            .toString()
+                            .replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+                        : "N/A"
+                    }</h5>
+                    </div>
+                    <div class="d-grid gap-2">
+                        <button class="btn btn-primary daftar-button" data-id="${
+                          course.id
+                        }" type="button">Daftar Sekarang</button>
+                    </div>
+                </div>
+            </div>`;
+    wrapper.appendChild(column);
+  }
+
+//   BUTTON DAFTAR
+  const buttons = document.querySelectorAll(".daftar-button");
+  buttons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      const courseId = event.target.getAttribute("data-id");
+      window.location.href = `product.html?id=${courseId}`;
+    });
+  });
+}
+
+// SEARCH FEATURE
+function filterCourses(keyword, page = 1) {
+  const filteredCourses = courses.filter((course) =>
+    course.title.toLowerCase().includes(keyword.toLowerCase())
+  );
+  displayList(filteredCourses, listElement, rows, columns, page);
+}
+
+function filter(keyword = "", page = 1) {
+  filterCourses(keyword, page);
+  displayPagination();
+}
+
+// PAGINATION
+function displayPagination() {
+  let totalPages = Math.ceil(courses.length / itemsPerPage);
+  paginationElement.innerHTML = "";
+
+  let prevButton = document.createElement("li");
+  prevButton.classList.add("page-item");
+  prevButton.innerHTML = `
+        <a class="page-link prev" tabindex="-1" aria-disabled="true">&#8592; Previous</a>
+    `;
+  paginationElement.appendChild(prevButton);
+
+  for (let i = 1; i <= totalPages; i++) {
+    let button = document.createElement("li");
+    button.classList.add("page-item");
+    button.innerHTML = `
+            <a class="page-link" href="#">${i}</a>
+        `;
+    paginationElement.appendChild(button);
+  }
+
+  let nextButton = document.createElement("li");
+  nextButton.classList.add("page-item");
+  nextButton.innerHTML = `
+        <a class="page-link next" href="#">Next &#8594;</a>
+    `;
+  paginationElement.appendChild(nextButton);
+}
+
+searchInput.addEventListener("input", function () {
+  filter(this.value);
+});
+
+paginationElement.addEventListener("click", function (event) {
+  if (event.target.tagName === "A") {
+    if (event.target.classList.contains("prev") && currentPage > 1) {
+      currentPage--;
+    } else if (
+      event.target.classList.contains("next") &&
+      currentPage < Math.ceil(courses.length / itemsPerPage)
+    ) {
+      currentPage++;
+    } else {
+      currentPage = parseInt(event.target.textContent);
+    }
+    displayList(courses, listElement, rows, columns, currentPage);
+  }
+});
+
+initializeCourses();
+
+module.exports = {displayList, displayPagination, filterCourses, filter}
---
+++ product.js
@@ -0,0 +1,150 @@
+const fasilitas = [
+  {
+    image: "./asset/fasilitas/komunitas.png",
+    title: "Komunitas",
+    text: "Kenali banyak anggota dari komunitas alumni Edspert dari berbagai macam bidang studi.",
+  },
+  {
+    image: "./asset/fasilitas/1on1.png",
+    title: "1-on-1 Session",
+    text: "Kenali banyak anggota dari komunitas alumni Edspert dari berbagai macam bidang studi.",
+  },
+  {
+    image: "./asset/fasilitas/active-learning.png",
+    title: "Active Learning",
+    text: "Kenali banyak anggota dari komunitas alumni Edspert dari berbagai macam bidang studi.",
+  },
+  {
+    image: "./asset/fasilitas/happy-atmosphere.png",
+    title: "Happy Atmosphere",
+    text: "Kenali banyak anggota dari komunitas alumni Edspert dari berbagai macam bidang studi.",
+  },
+  {
+    image: "./asset/fasilitas/tugas.png",
+    title: "Tugas",
+    text: "Kenali banyak anggota dari komunitas alumni Edspert dari berbagai macam bidang studi.",
+  },
+  {
+    image: "./asset/fasilitas/intensive-bootcamp.png",
+    title: "Intensive Bootcamp",
+    text: "Kenali banyak anggota dari komunitas alumni Edspert dari berbagai macam bidang studi.",
+  },
+];
+
+// AMBIL PARAMS URL
+const getQueryParam = (param) => {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(param);
+};
+
+const courseId = getQueryParam("id");
+
+// MENTOR
+// const courseId = 2;
+
+const detailCourse = async () => {
+  try {
+    const response = await axios.get(
+      `https://be-edspert.vercel.app/api/products/`
+    );
+    const courses = response.data;
+    // console.log('courses', courses);
+    mentorList(courses, courseId);
+    materi(courses, courseId);
+    bannerTitle(courses, courseId);
+    webTitleTab(courses, courseId);
+  } catch (error) {
+    console.error("Error fetching course details:", error);
+  }
+};
+// BANNER
+const title = document.getElementById("banner-title");
+const bannerTitle = (courses, courseId) => {
+  const filteredCourse = courses.products[courseId - 1];
+  // console.log('filteredCourseTitle', filteredCourse);
+  title.innerHTML = filteredCourse.title;
+};
+
+// TITLE HEAD
+const webTitle = document.getElementById("website-title");
+const webTitleTab = (courses, courseId) => {
+  const filteredCourse = courses.products[courseId - 1];
+  console.log("filteredCourseTitleTab", filteredCourse);
+  webTitle.innerHTML = filteredCourse.title;
+};
+
+// webTitleTab(courses, courseId)
+
+// MENTOR
+const mentorList = (courses, courseId) => {
+  const filteredCourse = courses.products[courseId - 1];
+  console.log("courses", courses);
+  console.log("courseId", courseId);
+  console.log("filteredCourse", filteredCourse);
+
+  const cardMentors = document.querySelectorAll(".card-mentor");
+
+  filteredCourse.mentor.forEach((mentor, index) => {
+    const randPhoto = Math.floor(Math.random() * 8) + 1;
+    const card = cardMentors[index];
+    card.innerHTML = `
+      <div class="card" style="width: 18rem;height:25rem">
+        <img src="./asset/mentor/Avatar-${randPhoto}.png" class="card-img-top" alt="Foto Mentor">
+        <div class="card-body">
+          <h5 class="card-title">${mentor}</h5>
+          <h6 class="card-title">${filteredCourse.mentorJabatan[index]}</h6>
+          <p class="card-text">${filteredCourse.mentorText[index]}</p>
+          <div class="card-sosmed">
+            <img src="./asset/twitter.png">
+            <img src="./asset/linkedin.png">
+            <img src="./asset/website.png">
+          </div>
+        </div>
+      </div>
+    `;
+  });
+};
+
+// FACILITY
+const cardFacility = document.querySelectorAll(".card-facility");
+
+let index = 0;
+for (let facilityIndex = 0; facilityIndex < fasilitas.length; facilityIndex++) {
+  cardFacility.forEach((card) => {
+    if (index < fasilitas.length) {
+      card.innerHTML += `
+                <div class="card" style="width: 18rem;">
+                    <img src="${fasilitas[index].image}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${fasilitas[index].title}</h5>
+                        <p class="card-text">${fasilitas[index].text}</p>
+                    </div>
+                </div>
+            `;
+      index++;
+    }
+  });
+}
+
+// MATERI
+
+const materi = (courses, courseId) => {
+  const filteredCourse = courses.products[courseId - 1];
+  // console.log('filteredCourseMateri', filteredCourse.materiText);
+  const materiContainer = document.getElementById("materi-container");
+
+  filteredCourse.materi.forEach((materi, index) => {
+    const materiDiv = document.createElement("div");
+    materiDiv.classList.add("flex", "flex-column", "materi-list");
+    materiDiv.innerHTML = `
+      <h5 class="list-title">${materi}</h5>
+      <h6 class="list-text">${filteredCourse.materiText[index]}</h6>
+    `;
+    materiContainer.appendChild(materiDiv);
+  });
+};
+
+detailCourse();
+
+
+module.exports={detailCourse, mentorList, materi, bannerTitle, webTitleTab}
