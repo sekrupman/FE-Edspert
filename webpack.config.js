491d29074f78e8b53070100a6daaaa0541e2deb4
sekrupman <45067224+sekrupman@users.noreply.github.com>
2024-05-19 18:36:49 +0700

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,14 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'production',
+  mode: 'production',
   entry: {
     katalog: './src/katalog.js',
     product: './src/product.js'
   },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public'), 
     clean: true
   },
   module: {
@@ -28,17 +28,21 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      filename: 'katalog.html',
-      template: './src/katalog.html',
+      filename: 'katalog.html', 
+      template: path.resolve(__dirname, './src/katalog.html'),
       chunks: ['katalog']
     }),
     new HtmlWebpackPlugin({
       filename: 'product.html',
-      template: './src/product.html',
-      chunks: ['product']
+      template: path.resolve(__dirname, './src/product.html'),
+      chunks: ['product'] 
     })
   ],
   devServer: {
-    contentBase: './dist'
+    static: {
+      directory: path.resolve(__dirname, 'public'),
+    },
+    port: 3000,
+    open: true
   }
 };
