1100b2dd0228c7c6c2094e8ee264c29679b43372
sekrupman <45067224+sekrupman@users.noreply.github.com>
2024-05-19 17:49:08 +0700

    edit  webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public'),
     clean: true
   },
   module: {
@@ -38,6 +38,6 @@ module.exports = {
     })
   ],
   devServer: {
-    contentBase: './dist'
+    contentBase: './public'
   }
 };
