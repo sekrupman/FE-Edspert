0e8840f9cf0bea90aa26623f53c74187737bb577
sekrupman <45067224+sekrupman@users.noreply.github.com>
2024-05-19 17:38:29 +0700

    build webpack

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    katalog: './src/katalog.js',
+    product: './src/product.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'katalog.html',
+      template: './src/katalog.html',
+      chunks: ['katalog']
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'product.html',
+      template: './src/product.html',
+      chunks: ['product']
+    })
+  ],
+  devServer: {
+    contentBase: './dist'
+  }
+};
